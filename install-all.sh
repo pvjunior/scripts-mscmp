068b4a9ca9ad130eb67e1cb2aebc3b13eb613cf8
pvjunior <pvjunior@inf.ufpel.edu.br>
2026-06-19 15:59:58 -0300

    install-all

---
+++ install-all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+bash browsers-set.sh
+bash hosts-set.sh
+
+echo "Configurações aplicadas com sucesso."
---
+++ remove-all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+bash browsers-rm.sh
+bash hosts-rm.sh
+
+echo "Configurações removidas com sucesso."
